b3a4def55b78cf71e98ef60b81dc41847a44aaf7
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 20:42:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Question 06:
+// Stripping Name: Store a person's name & include some white space characters at the beginning & end of the name. Make sure you use each character combination, "\t" & "\n" ,at least onece.Print the name once,so the white space around the name is displayed. Then print the name after stripping the white spaces.
+//Answer:
+var personName = "\t\n John doe \n\t";
+console.log("original :", personName);
+console.log("stripping:", personName.trim());
